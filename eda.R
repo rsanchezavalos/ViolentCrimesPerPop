f7a9d041d6d8b885e685193967feefdd6a6df2b5
rsanchezavalos <r.sanchezavalos@gmail.com>
2016-12-07 09:10:02 -0600

    initial init

---
+++ eda.R
@@ -0,0 +1,11 @@
+library(repmis)
+
+source_data("https://github.com/jcmartinezovando/est46111_datos/blob/master/EST46111_NFLDraft_Data.RData?raw=true")
+source_data("https://github.com/jcmartinezovando/est46111_datos/blob/master/EST46111_DatosMexico_Data.RData?raw=true")
+source_data("https://github.com/jcmartinezovando/est46111_datos/blob/master/EST46111_Donohue&Levitt_Data.RData?raw=true")
+source_data("https://github.com/jcmartinezovando/est46111_datos/blob/master/EST46111_MEXBaseline_Data.RData?raw=true")
+source_data("https://github.com/jcmartinezovando/est46111_datos/blob/master/EST46111_USCommData_Data.RData?raw=true")
+
+source("https://raw.githubusercontent.com/jcmartinezovando/est46111_datos/master/BayLinReg.R")
+source("https://raw.githubusercontent.com/jcmartinezovando/est46111_datos/master/BayHierLinReg.R")
+
